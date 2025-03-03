b91e89b6c2adcb3d99d9ce2e80b1977acadfd6de
isa134340 <mherrera35@alumnos.uaq.mx>
2025-03-03 15:17:33 -0600

    a ver

--- scripts/cabeza_coco.R
+++ scripts/cabeza_coco.R
@@ -2,5 +2,12 @@
 #nada perre
 #te gustan chacales o q?
 #cual es la diferencia de un chacal con un miembro productivo de la sociedad
+
+
+
+
+
+
 #los chacales son muy reconocibles, pq son malandros y buenotes
 #qpd con esta mmasa
+#a ver así
