7507e6f38c41961596ada06a882a0046fcb3501f
jul-goher <juliegonzalezhrra@gmail.com>
2025-03-03 14:58:34 -0600

    Hice un script de instrucciones que utilicé para el git

---
+++ scripts/git_installation.R
@@ -0,0 +1,11 @@
+#Git en ar hivos de programa
+library (usethis)
+
+usethis::git_sitrep()
+
+usethis::use_git_config(user.name = 'jul-goher', 
+                        user.email = 'juliegonzalezhrra@gmail.com')
+
+#Cree un repositorio en github :
+#https://github.com/jul-goher/repos_prueba.git
+#Me metí a la ventana derecha superior y de ahí elegí un nuevo proyecto, seleccioné git y puse el https 
